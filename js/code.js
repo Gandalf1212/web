164c92eb7bcdf6c8e25eaac8cc90df3cde864d9e
Gandalf1212 <71775953+Gandalf1212@users.noreply.github.com>
2021-11-26 11:39:24 +0300

    Add files via upload

--- js/code.js
+++ js/code.js
@@ -1,3 +1,25 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
   /*timeStart();
   function timeStart() {
     if (document.readyState == "loading"){
@@ -15,11 +37,67 @@
     return false;
   }*/
 
-timeCalc()
+/* timeCalc()
 
 function timeCalc() {
   console.log(window.performance.timing);
   let a = window.performance.unloadEventEnd;
   console.log(a);
-  console.log(window.performance.unloadEventEnd - window.performance.navigationStart);
+  console.log(window.performance.onloadEventEnd - window.performance.navigationStart);
 }
+*/
+/*
+var form = document.getElementById("createT");
+form.addEventListener("submit", getFormValue());
+var data
+
+function getFormValue(event) {
+    //event.preventDefault;
+    let line = form.elements.lines;
+    let column = form.elements.column;
+    data = {
+      lines: line.value,
+      columns: column.value
+    }
+}
+
+console.log(data);
+console.log("fghj");
+
+
+
+
+function createTable(l, c, p) {
+  let table = document.createElement('table');
+  let thead = document.createElement('thead');
+  let tbody = document.createElement('tbody');
+  let th = document.createElement('tr');
+  let tr = document.createElement('th');
+  let td = document.createElement('td');
+
+  table.appendChild(thead);
+
+  table.appendChild(tbody);
+
+  p.appendChild(table);
+}
+
+
+
+
+
+table.appendChild(tbody);
+for (let i = 0; i < l, i++) {}
+  let tr = document.createElement('tr');
+
+  for (let j = 0; j < c; j++){
+    tr.appendChild(document.createElement('td'));
+  }
+  thbody.appendChild(tr);
+}
+
+
+p.appendChild(table);
+}
+
+*/
